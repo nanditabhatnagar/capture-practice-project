b899e72d19685b8282f2c8647b547cec6da085ac
Nandita Bhatnagar <nandita.bhatnagar@imagr.co>
2021-01-15 10:03:21 +1300

    mainfile-updates

--- main.py
+++ main.py
@@ -5,6 +5,10 @@ from fastapi.templating import Jinja2Templates
 
 app = FastAPI()
 
-app.mount("/static", StaticFiles(directory="static"), name="static")
+app.mount("/static/small", StaticFiles(directory="static/small"), name="small")
 
 templates = Jinja2Templates(directory="templates")
+
+@app.get("/barcodes/{barcode}", response_class=HTMLResponse)
+async def read_item(request: Request, barcode: str):
+    return templates.TemplateResponse("barcodes.html", {"request": request, "barcode": barcode})
