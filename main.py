3ba56d6e5e65ce514c2ef9d1e4cd45875c33774b
Nandita Bhatnagar <nandita.bhatnagar@imagr.co>
2021-01-15 12:18:01 +1300

    loop.html-added

--- main.py
+++ main.py
@@ -5,14 +5,20 @@ from fastapi.templating import Jinja2Templates
 
 app = FastAPI()
 
-app.mount("/static", StaticFiles(directory="static/small"), name="static")
+app.mount("/static", StaticFiles(directory="static"), name="static")
 
 templates = Jinja2Templates(directory="templates")
 
 @app.get("/barcodes/{barcode}", response_class=HTMLResponse)
 async def read_item(request: Request, barcode: str):
-    return templates.TemplateResponse("index.html", {"request": request, "barcode": barcode})
+    return templates.TemplateResponse("barcode.html", {"request": request, "barcode": barcode})
 
-@app.get("/index", response_class=HTMLResponse)
-async def root():
-    return templates.TemplateResponse("index.html")
+@app.get("/index/", response_class=HTMLResponse)
+async def read_item(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request})
+
+
+@app.get("/loop", response_class=HTMLResponse)
+async def loop(request: Request):
+    stuff = [i**2 for i in range(1000)]
+    return templates.TemplateResponse("loop.html", {"request": request, "stuff": stuff})
