b73e8040915f7f114d34d5dccfbdf7d585030bf6
Nandita Bhatnagar <nandita.bhatnagar@imagr.co>
2021-01-15 11:33:30 +1300

    index.html-included-trying-to-work-links

--- main.py
+++ main.py
@@ -5,10 +5,14 @@ from fastapi.templating import Jinja2Templates
 
 app = FastAPI()
 
-app.mount("/static/small", StaticFiles(directory="static/small"), name="small")
+app.mount("/static", StaticFiles(directory="static/small"), name="static")
 
 templates = Jinja2Templates(directory="templates")
 
 @app.get("/barcodes/{barcode}", response_class=HTMLResponse)
 async def read_item(request: Request, barcode: str):
-    return templates.TemplateResponse("barcodes.html", {"request": request, "barcode": barcode})
+    return templates.TemplateResponse("index.html", {"request": request, "barcode": barcode})
+
+@app.get("/index", response_class=HTMLResponse)
+async def root():
+    return templates.TemplateResponse("index.html")
