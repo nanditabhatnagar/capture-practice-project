753ce5f1d4e5f8267b30ab5e3d6ae0152088a4f9
Nandita Bhatnagar <nandita.bhatnagar@imagr.co>
2021-01-20 11:54:12 +1300

    modified index with working drop-down box for sorting options

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI, Request
 from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
+from typing import Optional
 import os, sys
 
 app = FastAPI()
@@ -9,6 +10,14 @@ app = FastAPI()
 app.mount("/static", StaticFiles(directory="static"), name="static")
 
 templates = Jinja2Templates(directory="templates")
+
+#this function sorts a list of folders in ascending order, depending on how many files are in each folder
+def sort_files(barcodeList, file_count):
+    bigPicture = zip(barcodeList, file_count)
+    bigPicture = list(bigPicture)
+    barcodeList = sorted(bigPicture, key = lambda x: x[1]) 
+    barcodeList = list(zip(*barcodeList))
+    return list(barcodeList[0])
 
 #root endpoint
 @app.get("/")
@@ -29,13 +38,46 @@ async def read_item(request: Request, barcode: str):
     return templates.TemplateResponse("barcode.html", {"request": request, "barcode" : barcode, "imageDetails": imageDetails})
 
 @app.get("/index/", response_class=HTMLResponse)
-async def read_item(request: Request):
+async def read_item(request: Request, sortingby: Optional[str] = None):
+    #if the user wants to sort the index by the number of images per barcode folder in ascending order
+    if sortingby == 'numfilesasc': 
+        barcodeList = []
+        file_count = []
+        for x in os.listdir('./static/small'):
+            barcodeList.append(x)
+            path = f"./static/small/{x}"
+            file_counter = len(os.listdir(path)) #counting no. of files in each folder
+            file_count.append(file_counter)
+        barcodeList = sort_files(barcodeList, file_count)
+        return templates.TemplateResponse("index.html", {"request": request, "barcodeList": barcodeList})
+    
+    #if the user wants to sort the index by the number of images per barcode folder in descending order
+    if sortingby == 'numfilesdesc': 
+        barcodeList = []
+        file_count = []
+        for x in os.listdir('./static/small'):
+            barcodeList.append(x)
+            path = f"./static/small/{x}"
+            file_counter = len(os.listdir(path)) #counting no. of files in each folder
+            file_count.append(file_counter)
+        barcodeList = sort_files(barcodeList, file_count)
+        barcodeList.reverse()
+        return templates.TemplateResponse("index.html", {"request": request, "barcodeList": barcodeList})
+    
+    #if the user wants to sort the index by barcode number in descending order
+    if sortingby == 'barcodedesc': 
+        barcodeList = []
+        file_count = []
+        for x in os.listdir('./static/small'):
+            barcodeList.append(x)
+            barcodeList.sort(key = int)
+        barcodeList.reverse()
+        return templates.TemplateResponse("index.html", {"request": request, "barcodeList": barcodeList})
+    
+    #default option: the user wants to sort the index by barcode number in ascending order
     barcodeList = []
+    file_count = []
     for x in os.listdir('./static/small'):
         barcodeList.append(x)
+        barcodeList.sort(key = int)   
     return templates.TemplateResponse("index.html", {"request": request, "barcodeList": barcodeList})
-
-@app.get("/loop", response_class=HTMLResponse)
-async def loop(request: Request):
-    stuff = [i**2 for i in range(1000)]
-    return templates.TemplateResponse("loop.html", {"request": request, "stuff": stuff})
---
+++ test_sorting.py
@@ -0,0 +1,31 @@
+import unittest
+from main import sort_files
+
+class TestSorting(unittest.TestCase):
+
+    def test_sort_by_file_count(self):
+        barcodeList = ['1','2','3','4','5']
+        file_count = [10,9,8,7,6]
+        sortedFileList = sort_files(barcodeList, file_count)
+        expected = ['5','4','3','2','1']
+        self.assertIsInstance(sortedFileList, list)
+        self.assertEqual(sortedFileList, expected)
+ 
+    def test_sort_by_file_count2(self):
+        barcodeList = ['1','2','3','4','5']
+        file_count = [100,19,58,78,16]
+        sortedFileList = sort_files(barcodeList, file_count)
+        expected = ['5','2','3','4','1']
+        self.assertIsInstance(sortedFileList, list)
+        self.assertEqual(sortedFileList, expected)
+ 
+    def test_sort_by_file_count3(self):
+        barcodeList = ['1','2','3','4','5']
+        file_count = [10,9,8,7,6]
+        sortedFileList = sort_files(barcodeList, file_count)
+        expected = ['5','4','3','2','1']
+        self.assertIsInstance(sortedFileList, list)
+        self.assertEqual(sortedFileList, expected)
+
+if __name__ == '__main__':
+    unittest.main()
