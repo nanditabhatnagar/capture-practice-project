c0c7aaadc5831b5e79ece23cb327a7bc1604d132
AmyLyu <amy.lyu@imagr.co>
2021-01-19 14:55:20 +1300

    Incomplete sorting option feature

---
+++ static/barcodeScript.js
@@ -0,0 +1,22 @@
+/* When the user clicks on the button, 
+toggle between hiding and showing the dropdown content */
+function showTypes() {
+    document.getElementById("sortType").classList.toggle("show");
+  }
+  
+// Close the dropdown if the user clicks outside of it
+window.onclick = function(event) {
+    if (!event.target.matches('.sort-button')) {
+        var types = document.getElementsByClassName("sort-type");
+        for (var i = 0; i < types.length; i++) {
+            var openTypes = types[i];
+            if (openTypes.classList.contains('show')) {
+            openTypes.classList.remove('show');
+            }
+        }
+    }
+}
+
+function sortSelectOption(option) {
+    document.getElementById("sortButton").textContent = "Sorted by " + option
+}
